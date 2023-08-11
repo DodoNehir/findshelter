030b883c30dd4494898fcddb7bb785e2cdc96443
unknown <rojalaurant@gmail.com>
2023-08-12 00:27:50 +0900

    add fun '내 위치 찾기'

--- app/src/main/java/com/example/findshelter/MainActivity.kt
+++ app/src/main/java/com/example/findshelter/MainActivity.kt
@@ -1,21 +1,39 @@
 package com.example.findshelter
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
+import android.widget.Toast
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
+import androidx.core.content.ContextCompat
+import com.example.findshelter.PermissionUtils.PermissionDeniedDialog.Companion.newInstance
+import com.example.findshelter.PermissionUtils.isPermissionGranted
 import com.example.findshelter.databinding.ActivityMainBinding
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 
-private lateinit var binding: ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback{
+class MainActivity : AppCompatActivity(),
+    OnMyLocationButtonClickListener,
+    OnMyLocationClickListener,
+    OnMapReadyCallback,
+    OnRequestPermissionsResultCallback {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var mMap: GoogleMap
+    private var permissionDenied = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,9 +42,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback{
         setContentView(binding.root)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager.findFragmentById(binding.root.id) as SupportMapFragment
+        val mapFragment =
+            supportFragmentManager.findFragmentById(binding.root.id) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
     }
 
 
@@ -39,15 +57,154 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback{
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    override fun onMapReady(p0: GoogleMap) {
-        mMap = p0
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        googleMap.setOnMyLocationButtonClickListener(this)
+        googleMap.setOnMyLocationClickListener(this)
+        initMap()
+        enableMyLocation()
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions()
-            .position(sydney)
-            .title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    /**
+     * FINE 위치 권한 있을 때
+     */
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+
+        //1. 권한 있는지 확인
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
+            return
+        }
+
+        // 2. 다이얼로그 띄워야한다면 띄우기
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
+            PermissionUtils.RationaleDialog.newInstance(
+                LOCATION_PERMISSION_REQUEST_CODE, true
+            ).show(supportFragmentManager, "dialog")
+            return
+        }
+
+        // 3. Otherwise, request permission
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
+        // [END maps_check_location_permission]
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "내 위치 찾기", Toast.LENGTH_SHORT)
+            .show()
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false
+    }
+
+    override fun onMyLocationClick(location: Location) {
+        Toast.makeText(this, "현재 위치:\n$location", Toast.LENGTH_LONG)
+            .show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            return
+        }
+
+        if (isPermissionGranted(
+                permissions,
+                grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) || isPermissionGranted(
+                permissions,
+                grantResults,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
+            enableMyLocation()
+        } else {
+            permissionDenied = true
+        }
+    }
+
+    // [END maps_check_location_permission_result]
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (permissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError()
+            permissionDenied = false
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private fun showMissingPermissionError() {
+        newInstance(true).show(supportFragmentManager, "dialog")
+    }
+
+    /**
+     * 기본 위치 설정
+     */
+    private fun initMap() {
+        //최소, 최대 확대 수준을 설정
+        mMap.setMinZoomPreference(8.0f)
+        mMap.setMaxZoomPreference(20.0f)
+
+        //TODO:이전 위치를 찾았다면 그 곳이 나오도록. 지금은 계속 첫 위치인 잠실한강공원이 나온다.
+
+        // Set the map coordinates to Kyoto Japan.
+        val jamsilPark = LatLng(37.517588, 127.086711)
+
+        // Set the map type to Hybrid.
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+        // Add a marker on the map coordinates.
+        mMap.addMarker(
+            MarkerOptions()
+                .position(jamsilPark)
+                .title("잠실한강공원")
+        )
+
+        // Move the camera to the map coordinates and zoom in closer.
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(jamsilPark))
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(17f))
+
+        // Display traffic.
+        mMap.isTrafficEnabled = true
+    }
+
+    companion object {
+        /**
+         * Request code for location permission request.
+         *
+         * @see .onRequestPermissionsResult
+         */
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
 
 }
