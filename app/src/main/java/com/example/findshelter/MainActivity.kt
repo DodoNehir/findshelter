d37472b76411703bad9bd7e74beb30a17d39208a
unknown <rojalaurant@gmail.com>
2023-08-18 17:51:22 +0900

    show marker on the map

--- app/src/main/java/com/example/findshelter/MainActivity.kt
+++ app/src/main/java/com/example/findshelter/MainActivity.kt
@@ -6,15 +6,13 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.widget.Toast
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
 import androidx.core.content.ContextCompat
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
 import com.example.findshelter.PermissionUtils.PermissionDeniedDialog.Companion.newInstance
 import com.example.findshelter.PermissionUtils.isPermissionGranted
 import com.example.findshelter.databinding.ActivityMainBinding
@@ -28,13 +26,19 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.navigation.NavigationView
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
+import javax.xml.parsers.DocumentBuilderFactory
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity(),
     OnMyLocationButtonClickListener,
     OnMyLocationClickListener,
     OnMapReadyCallback,
     OnRequestPermissionsResultCallback,
-    NavigationView.OnNavigationItemSelectedListener{
+    NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var mMap: GoogleMap
@@ -53,30 +57,107 @@ class MainActivity : AppCompatActivity(),
         mapFragment.getMapAsync(this)
 
         initNavigationMenu()
+
+        startGetApi()
     }
 
     private fun initNavigationMenu() {
         binding.mainNavigationView.setNavigationItemSelectedListener(this)
 
-        toggle = ActionBarDrawerToggle(this, binding.drawerLayout,
-            R.string.drawer_opened, R.string.drawer_closed)
+        toggle = ActionBarDrawerToggle(
+            this, binding.drawerLayout,
+            R.string.drawer_opened, R.string.drawer_closed
+        )
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toggle.syncState()
     }
 
+    private fun startGetApi() {
+        val serviceKey =
+            "?serviceKey=ANkRyAgZUuuFouNIBZiN%2F9cLuafMaWihg4rYPimPNJsBpTlR3uAsXr%2BJb3KfOVwNLwngOK5O2SU%2BI1C7OW0ZZw%3D%3D"
+        val pageNo = "&pageNo=1"
+        val numOfRows = "&numOfRows=5"
+        val type = "&type=XML"
+        val year = "&year=2022"
+        val areaCd = "&areaCd=1111064000"
+        val equptype = "&equptype=001"
+        /*001:노인시설 002:복지회관 003:마을회관 004:보건소 005:주민센터 006:면동사모소 007:종교시설
+        008:금융기관 009:정자 010:공원 011:정자,파고라 012:공원 013:교량하부 014:나무그늘 015:하천둔치 099:기타*/
+
+        // api 정보를 가지고있는 주소
+        var url = "http://apis.data.go.kr/1741000/HeatWaveShelter2/getHeatWaveShelterList2"
+        url = url + serviceKey + pageNo + numOfRows + type + year + areaCd + equptype
+
+        thread(start = true) {
+            Log.d("API로그", "스레드 시작")
+
+            try {
+                //XML 문서 빌더 객체 생성
+                val xml: Document =
+                    DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(url)
+                xml.documentElement.normalize()
+
+                //row 태그를 가지는 노드를 찾는다. 계층적인 노드 구조를 반환한다.
+                val list: NodeList = xml.getElementsByTagName("row")
+                Log.d("API로그", "list row 갯수: " + list.length)
+
+                //row 태그의 정보를 가져온다
+                for (i in 0..list.length - 1) {
+                    val n: Node = list.item(i)
+
+                    if (n.nodeType == Node.ELEMENT_NODE) {
+                        val elem = n as Element
+
+                        Log.d("API로그", "${i + 1}번째 데이터")
+                        Log.d(
+                            "API로그",
+                            "${i}" + " 번째 item에서 " + elem.getElementsByTagName("lo")
+                                .item(0).textContent
+                        )
+
+                        var position = LatLng(
+                            elem.getElementsByTagName("la").item(0).textContent.toDouble(),
+                            elem.getElementsByTagName("lo").item(0).textContent.toDouble()
+                        )
+
+                        runOnUiThread {
+                            // for문 돌면서 위치 마커 추가
+                            mMap.addMarker(
+                                MarkerOptions()
+                                    .position(position)
+                                    .title(
+                                        elem.getElementsByTagName("restname").item(0).textContent
+                                    )
+                            )
+                            //TODO: 매번 카메라를 위치시키지 않을 방법?
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(position))
+                        }
+                    }
+                }
+                runOnUiThread {
+                    //for문 끝난 뒤 카메라 이동
+                    mMap.moveCamera(CameraUpdateFactory.zoomTo(17f))
+                }
+            } catch (e: Exception) {
+                Log.d("API로그", "StartgetAPI thread e: " + e.toString())
+            }
+        }
+    }
+
     //토글 버튼을 누르면 Drawer가 들어가거나 나간다
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(toggle.onOptionsItemSelected(item)) {
+        if (toggle.onOptionsItemSelected(item)) {
             return true
         }
         return super.onOptionsItemSelected(item)
     }
 
+    //TODO 버튼 선택 시 해당 위치가 표시되도록 만들어야 함
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.item1_tree -> Toast.makeText(this,"나무", Toast.LENGTH_SHORT).show()
-            R.id.item2_park -> Toast.makeText(this,"공원", Toast.LENGTH_SHORT).show()
-            R.id.item3_building -> Toast.makeText(this,"빌딩", Toast.LENGTH_SHORT).show()
+        when (item.itemId) {
+            R.id.item1_tree -> Toast.makeText(this, "나무", Toast.LENGTH_SHORT).show()
+            R.id.item2_park -> Toast.makeText(this, "공원", Toast.LENGTH_SHORT).show()
+            R.id.item3_building -> Toast.makeText(this, "빌딩", Toast.LENGTH_SHORT).show()
         }
         return false
     }
@@ -201,34 +282,13 @@ class MainActivity : AppCompatActivity(),
     }
 
     /**
-     * 기본 위치 설정
+     * 기본 지도 설정
      */
     private fun initMap() {
         //최소, 최대 확대 수준을 설정
         mMap.setMinZoomPreference(8.0f)
         mMap.setMaxZoomPreference(20.0f)
-
-        //TODO:이전 위치를 찾았다면 그 곳이 나오도록. 지금은 계속 첫 위치인 잠실한강공원이 나온다.
-
-        // Set the map coordinates to Kyoto Japan.
-        val jamsilPark = LatLng(37.517588, 127.086711)
-
-        // Set the map type to Hybrid.
         mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
-
-        // Add a marker on the map coordinates.
-        mMap.addMarker(
-            MarkerOptions()
-                .position(jamsilPark)
-                .title("잠실한강공원")
-        )
-
-        // Move the camera to the map coordinates and zoom in closer.
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(jamsilPark))
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(17f))
-
-        // Display traffic.
-        mMap.isTrafficEnabled = true
     }
 
     companion object {
